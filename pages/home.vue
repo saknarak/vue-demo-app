034c236c5edc4f62d8571bb8618c1e406f77b8a2
tbinkasem <tbinkasem@gmail.com>
2018-05-03 14:00:01 +0700

    test commit

--- pages/home.vue
+++ pages/home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
       <h1>Test Worawek</h1>
+      <h2>Test Teera</h2>
   </div>
 </template>
--- pages/somsak.vue
+++ pages/somsak.vue
@@ -1,3 +1,6 @@
 <template>
+<div>
   <h1>Somsak</h1>
+  <h2>TEERA</h2>
+  </div>
 </template>
