ae701655b1d550681ee7cc08ff50ac13ba6f8108
Somsak <saknarak@gmail.com>
2018-05-03 10:32:23 +0700

    commit

---
+++ pages/menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Menu</h1>
+    <ul>
+      <li><button @click="gotoRandom">Random</button></li>
+      <li v-for="n in 8" :key="n"><nuxt-link :to="'/sample0' + n">Sample0{{n}}</nuxt-link></li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    gotoRandom() {
+      // this.$router.push('/sample01')
+      // this.$router.replace('/sample01')
+      this.$router.go(-1)
+
+      // Home > About
+      // Main > About
+    }
+  }
+}
+</script>
